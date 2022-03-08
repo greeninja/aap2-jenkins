2959653cc617aeec0373beed84a14897f3cb5f96
Nick Campion <nickcampion88@gmail.com>
2022-03-08 14:55:43 +0000

    wip

--- scripts/start_job.sh
+++ scripts/start_job.sh
@@ -14,6 +14,8 @@ do
     esac
 done
 
+echo -e "###########\n# Summary #\n###########\n"
+
 echo "Token: $token"
 echo "Packages: $packages"
 echo "Inventory: $inventory"
@@ -21,6 +23,8 @@ echo "Tower Host: $tower"
 echo "Job ID: $jobid"
 
 # Check for Curl
+
+echo -e "$(date) - Check Curl & JQ available"
 
 if ! command -v curl &> /dev/null
 then
@@ -36,12 +40,18 @@ then
     exit
 fi
 
+echo -e "$(date) - Build data"
+
+echo -e "$(date) - Get inventory ID from flags"
 # Build Data
 ## Get inventory ID
 invId=$(echo $inventory | grep -o -P '(?<=\{).*(?=\=)')
 
+echo -e "$(date) - Build package array from flags"
 ## Build array of packages
 IFS=, read -ra pkgs <<< "$packages"
+
+echo -e "$(date) - Build JSON POST data"
 
 ## Build Post data
 pos=${pkgs[-1]}
@@ -55,13 +65,18 @@ $(for i in ${pkgs[@]}; do echo "\"$i\""; if [[ $i == $pos ]]; then echo ""; else
 EOF
 )
 
+echo -e "$(date) - Confirm POST data is valid"
 json=$(echo -e "$yml" | jq)
+
+echo -e "$(date) - Send Start Job request"
 
 response=$(curl -s --location --request POST "https://$tower/api/v2/job_templates/$jobid/launch/" \
   --header "Authorization: Bearer $token" \
   --header "Content-Type: application/json" \
   --header "Accept: application/json" \
   --data-raw "$(echo $json)")
+
+echo -e "$(date) - Tower response"
 
 echo $response | jq
 
